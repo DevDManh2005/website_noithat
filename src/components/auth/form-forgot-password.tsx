865f56d4002c3d66d91f835e0c092239b559d9e6
vuvansy <vuvansy3001@gmail.com>
2025-07-03 01:44:27 +0700

    Design Header & forgot-password

---
+++ src/components/auth/form-forgot-password.tsx
@@ -0,0 +1,41 @@
+
+
+import type { FormProps } from 'antd';
+import { Button, Form, Input } from 'antd';
+
+type FieldType = {
+    email?: string;
+};
+
+const ForgotPasswordForm = () => {
+    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <Form
+            className='max-w-[800px]'
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            layout='vertical'
+        >
+            <Form.Item<FieldType>
+                label="Email"
+                name="email"
+                rules={[{ required: true, message: 'Email không được để trống!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Button type="primary" htmlType="submit" className='w-full !bg-red1 !text-body-bold'>Gửi Yêu Cầu</Button>
+
+        </Form>
+    )
+}
+
+export default ForgotPasswordForm;
